0c8ff1806233dfddf2a3c212ca8431f76cf1148a
sadiqmuhammad795 <sadiqmuhammad795@gmail.com>
2022-02-24 12:13:07 -0700

    some changes

--- model/userModel.js
+++ model/userModel.js
@@ -1,13 +1,14 @@
+const mongoose = require("mongoose");
 
-const mongoose = require("mongoose"),
+// const mongoose = require("mongoose"),
     //Schema = mongoose.Schema,
-    autoIncrement = require('mongoose-auto-increment');
-    var connection = mongoose.createConnection("mongodb://localhost/Home_Tutors");
+//     autoIncrement = require('mongoose-auto-increment');
+//     var connection = mongoose.createConnection("mongodb://localhost/Home_Tutors");
  
-autoIncrement.initialize(connection);
+// autoIncrement.initialize(connection);
 const UserSchema = new mongoose.Schema(
   {
-    _id: {type: String, required: true},
+    // _id: {type: String, required: true},
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     mobile_no: { type: String},
@@ -48,16 +49,16 @@ const UserSchema = new mongoose.Schema(
     post_proposal : [
       {
         proposal_id : {type : String, default : "",}, 
-        pro_title: {type : String, default : "",}, 
-        pro_subject: {type : String, default : "",}, 
-        pro_depart:{type : String, default : "",}, 
-        pro_price : {type : String, default : "",},
-        pro_time : {type : String, default : "",}, 
-        pro_location : {type : String, default : "",}, 
-        pro_description : {type : String, default : "",},
-        pro_userData : {type : String, default : "",},
-        pro_geoLocation : {type : String, default : "",},
-        pro_Type : {type : String, default : "",},
+        post_title: {type : String, default : "",}, 
+        post_subject: {type : String, default : "",}, 
+        post_depart:{type : String, default : "",}, 
+        post_price : {type : String, default : "",},
+        post_time : {type : String, default : "",}, 
+        post_location : {type : String, default : "",}, 
+        post_description : {type : String, default : "",},
+        post_userData : {type : String, default : "",},
+        post_geoLocation : {type : String, default : "",},
+        post_Type : {type : String, default : "",},
       },
     ],
     
@@ -66,10 +67,10 @@ const UserSchema = new mongoose.Schema(
 );
 
 
-UserSchema.plugin(autoIncrement.plugin, {
-  model: 'Home-Tutors-Club',
-  field: '_id'
-});
+// UserSchema.plugin(autoIncrement.plugin, {
+//   model: 'Home-Tutors-Club',
+//   field: '_id'
+// });
 
 module.exports = mongoose.model("Home-Tutors-Club", UserSchema);
 
