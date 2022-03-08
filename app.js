2c93cd3238845bdfe5e7af3e83de4ba2d474e250
Saad Qureshi <saad.qureshi51.sq@gmail.com>
2022-03-08 22:19:30 +0500

    kham kha

--- app.js
+++ app.js
@@ -13,47 +13,47 @@ dotenv.config();
 const user = require("./model/userModel");
 
 //For image
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 const fs = require("fs");
 //const mongoose = require("mongoose");
-app.use(bodyParser.urlencoded(
- { extended:false }
-))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
 // SET STORAGE
 var storage = multer.diskStorage({
-destination: function (req, file, cb) {
- cb(null, 'uploads')
-},
-filename: function (req, file, cb) {
- cb(null, file.fieldname + '-' + Date.now())
-}
-})
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
 
-var upload = multer({ storage: storage })
+var upload = multer({ storage: storage });
 
-app.get("/",(req,res)=>{
-res.render("index");
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.post("/uploadphoto",upload.single('myImage'),(req,res,next)=>{
-var obj = {
- img: {
-     data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
-     contentType: 'image/png'
- }
-}
-user.create(obj,function(err,result){
-   if(err){
-       console.log(err);
-   }else{
-       res.redirect('/');
-   }
-})
-})
+app.post("/uploadphoto", upload.single("myImage"), (req, res, next) => {
+  var obj = {
+    img: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/uploads/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
+  user.create(obj, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -68,7 +68,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/userAuth", userAuth);
-app.use("/api/postAuth",postAuth);
+app.use("/api/postAuth", postAuth);
 app.listen(port, () => {
   console.log("Backend server is running on", port);
 });
